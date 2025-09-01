3cd621aa23b4c53acb3930be40edb0db3da4eaee
royboot <rgrizell467@gmail.com>
2025-09-01 18:28:09 +0200

    WIP started work on checkBans method

--- helpers/apiHelper.go
+++ helpers/apiHelper.go
@@ -34,7 +34,12 @@ func getFriends(steamid string, APIKey string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
 	body, _ := io.ReadAll(resp.Body)
 	var data FriendListResponse
 	if err := json.Unmarshal(body, &data); err != nil {
@@ -49,4 +54,19 @@ func getFriends(steamid string, APIKey string) ([]string, error) {
 func checkBans(friendids []string) ([]Playerban, error) {
 	url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=%s&steamids=%s", APIKey, strings.Join(steamids, ","))
 	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
+	var bans PlayerBansResponse
+	if err := json.Unmarshal(body, &bans); err != nil {
+		return nil, err
+	}
+	return bans.Players, nil
 }
