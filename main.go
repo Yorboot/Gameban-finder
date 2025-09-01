b651f5cc2fa6a0bf2d1839c21befb866fd388f1d
royboot <rgrizell467@gmail.com>
2025-09-01 18:09:54 +0200

    added fields to PlayerBan struct

--- main.go
+++ main.go
@@ -12,5 +12,11 @@ type FriendListResponse struct {
 	} `json:"friendlist"`
 }
 type PlayerBan struct {
-	Steamid string `json:"steamid"`
+	SteamId          string `json:"steamId"`
+	CommunityBanned  bool   `json:"community_banned"`
+	VACBanned        bool   `json:"vac_banned"`
+	NumberOfVACBans  int    `json:"NumberOfVACBans"`
+	NumberOfGameBans int    `json:"NumberOfGameBans"`
+	DaysSinceLastBan int    `json:"DaysSinceLastBan"`
+	EconomyBan       string `json:"EconomyBan"`
 }
