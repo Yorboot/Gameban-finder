5a184a0427767b1eb1c1409203caef46aef4afb0
royboot <rgrizell467@gmail.com>
2025-09-01 18:18:45 +0200

    added getFriends method to get the friends of a steamAcount

--- helpers/apiHelper.go
+++ helpers/apiHelper.go
@@ -1,9 +1,35 @@
 package helpers
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 type FriendListResponse struct {
 	Friendlist struct {
 		Friends []struct {
 			Steamid string `json:"steamid"`
 		} `json:"friends"`
 	} `json:"friendlist"`
+}
+
+func getFriends(steamid string, APIKey string) ([]string, error) {
+	url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=%s&relationship=friend", APIKey, steamid)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var data FriendListResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+	friends := []string{}
+	for _, friend := range data.Friendlist.Friends {
+		friends = append(friends, friend.Steamid)
+	}
+	return friends, nil
 }
