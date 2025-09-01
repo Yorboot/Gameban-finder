4a36fb72f01daac70e3adaed501f55b2acefaea7
royboot <rgrizell467@gmail.com>
2025-09-01 18:43:45 +0200

    WIP added test code for getting ui going

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package Gamebanfinder
+
+import (
+	//"Gamebanfinder/helpers"
+	"bufio"
+	"os"
+)
+
+func main() {
+	steamid := ""
+	for {
+		println("Welcome to Gamebanfinder import the steamid below")
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			steamid = input.Text()
+			println(steamid)
+		}
+	}
+	//Friends, err = getFriends()
+}
